125154a99b911d8afc58f36a878046a47681ac2c
letli <liletian@shidaixin.com>
2016-01-27 20:37:38 +0800

    用于参考

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#%pylab inline
-#sys.stdout = open('/dev/stdout', 'w')
-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys
 import copy
@@ -10,12 +8,21 @@ import matplotlib.pyplot as plt
 import networkx.algorithms.isomorphism as iso
 from itertools import chain, combinations
 
+
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    '''
+    产生一个数组所有的组合可能
+    powerset([1,2,3]) -> [() (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)]
+    '''
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 def Cache2Graph(c):
+    '''
+    操作gList中每一个Cache，按行解析成Graph
+
+    String -> Graph
+    '''
     Graph = nx.DiGraph()
     for line in c:
         p = line.split(',')
@@ -36,6 +43,11 @@ def Cache2Graph(c):
     return Graph
 
 def File2Graph():
+    '''
+    分割文件，缓存在gList中
+    
+    File -> gList [Str, ]
+    '''
     gList = []
     Cache = list()
     
@@ -55,18 +67,23 @@ def File2Graph():
         Cache.append(line)
     return gList
 
+'''
 def gList2gDict(gList):
     r = dict()
     for i, o in zip(range(len(gList)), gList):
         r[i] = dict()
         r[i]['origin'] = o
     return r
-
+'''
 
 suspicious = list()
-index = 0
-
 def isomorphism_set(sg):
+    '''
+    维护全局的不同构图集合，用于去重复
+
+    sg 新增图 Graph
+    suspicious 集合，全局变量 [Graph, ]
+    '''
     for s in suspicious:
         GM = nx.isomorphism.DiGraphMatcher(sg, s)
         if GM.is_isomorphic():
@@ -74,6 +91,12 @@ def isomorphism_set(sg):
     return False
 
 def supportValue(gList, sg):
+    '''
+    求支持数量，即gList中包含sg的图的数量
+
+    gList [Graph, ]
+    sg Graph
+    '''
     print '*** start ***'
     v = 0
     sptList = []
@@ -86,7 +109,13 @@ def supportValue(gList, sg):
     print '*** end.. ***'
     return v, sptList
 
+index = 0
 def main(seed_number, minspt):
+    '''
+    index 生成文件名计数 int
+    seed_number 作为图拆解种子的序号 int
+    minspt 最小支持数 int
+    '''
     global index
     global suspicious
     
@@ -144,149 +173,11 @@ def main(seed_number, minspt):
             somelist = [x for x in somelist if not set(nodesGroup).issubset(x)]            
     #return suspicious
 
-
-amount = len(File2Graph())
-for s in range(amount):
-    main(s, amount - 12)
-
-
-
-"""
-# *** 没有用的代码 *** #
-'''
-G=nx.DiGraph()
-G.add_node(1, time='A')
-G.add_node(2, time='B')
-G.add_edge(1,2)
-H=nx.DiGraph()
-H.add_node(2, time='B')
-H.add_node(1, time='A')
-H.add_node(13, time='A')
-H.add_edge(1,2)
-H.add_edge(1,3)
-H.nodes()
-K = H.subgraph((1,2))
-GM = nx.isomorphism.DiGraphMatcher(K, G)#, node_match=iso.categorical_node_match('time', None))
-GM.is_isomorphic()
-import networkx.algorithms.isomorphism as iso
-GM = nx.isomorphism.DiGraphMatcher(H, G)#, node_match=iso.categorical_node_match('time', None))
-GM.subgraph_is_isomorphic()
-ret = GM.subgraph_isomorphisms_iter()
-for i in ret:
-    print i
-for g in ret:
-    nx.draw(g)
-    plt.show()
-'''
+if __name__ == '__main__':
+    amount = len(File2Graph())
+    for s in range(amount):
+        main(s, amount - 12)
 
 
-def main():
-    gList = File2Graph()
-    somelist = [x for x in powerset(gList[4].nodes()) if len(x) > 1]
-
-    suspicious = list()
-    
-    while True:
-        if [] == somelist:
-            break
-        nodesGroup = somelist.pop(0)
-        print nodesGroup
-        
-        sg = gList[4].subgraph(nodesGroup)
-        '''
-        labels=dict((n,d['name']) for n,d in sg.nodes(data=True)) 
-        nx.draw(sg, labels=labels,node_size=1000)
-        plt.show()
-        raw_input()
-        '''
-        
-        GM1 = nx.isomorphism.DiGraphMatcher(gList[0], sg, node_match=iso.categorical_node_match('name', None))
-        GM2 = nx.isomorphism.DiGraphMatcher(gList[1], sg, node_match=iso.categorical_node_match('name', None))
-        GM3 = nx.isomorphism.DiGraphMatcher(gList[2], sg, node_match=iso.categorical_node_match('name', None))
-        GM4 = nx.isomorphism.DiGraphMatcher(gList[3], sg, node_match=iso.categorical_node_match('name', None))
-        if GM1.subgraph_is_isomorphic() and GM2.subgraph_is_isomorphic() and GM3.subgraph_is_isomorphic() and GM4.subgraph_is_isomorphic():
-            '''
-            for s in suspicious:
-                GM = nx.isomorphism.DiGraphMatcher(sg, s)
-                GM.is_isomorphic()
-                apd = 0
-                break
-                '''
-            suspicious.append(sg)
-            print "@@@: ", nodesGroup
-            print GM1.mapping
-            print GM2.mapping
-            print GM3.mapping
-            print GM4.mapping
-            print "##"
-        else:
-            somelist = [x for x in somelist if not set(nodesGroup).issubset(x)]            
-    return suspicious
-
-ret = main()
-for sg in ret:
-    labels=dict((n,d['name']) for n,d in sg.nodes(data=True)) 
-    nx.draw(sg, labels=labels,node_size=1000)
-    plt.show()
-def main():
-    gList = File2Graph()
-    somelist = [x for x in powerset(gList[4].nodes()) if len(x) > 1]
-
-    suspicious = list()
-    
-    while True:
-        if [] == somelist:
-            break
-        nodesGroup = somelist.pop(0)
-        print nodesGroup
-        
-        sg = gList[4].subgraph(nodesGroup)
-        '''
-        labels=dict((n,d['name']) for n,d in sg.nodes(data=True)) 
-        nx.draw(sg, labels=labels,node_size=1000)
-        plt.show()
-        raw_input()
-        '''
-        
-        GM1 = nx.isomorphism.DiGraphMatcher(gList[0], sg, node_match=iso.categorical_node_match('name', None))
-        GM2 = nx.isomorphism.DiGraphMatcher(gList[1], sg, node_match=iso.categorical_node_match('name', None))
-        GM3 = nx.isomorphism.DiGraphMatcher(gList[2], sg, node_match=iso.categorical_node_match('name', None))
-        GM4 = nx.isomorphism.DiGraphMatcher(gList[3], sg, node_match=iso.categorical_node_match('name', None))
-        if GM1.subgraph_is_isomorphic() and GM2.subgraph_is_isomorphic() and GM3.subgraph_is_isomorphic() and GM4.subgraph_is_isomorphic():
-            '''
-            for s in suspicious:
-                GM = nx.isomorphism.DiGraphMatcher(sg, s)
-                GM.is_isomorphic()
-                apd = 0
-                break
-                '''
-            suspicious.append(sg)
-            print "@@@: ", nodesGroup
-            print GM1.mapping
-            print GM2.mapping
-            print GM3.mapping
-            print GM4.mapping
-            print "##"
-        else:
-            somelist = [x for x in somelist if not set(nodesGroup).issubset(x)]            
-    return suspicious
-
-ret = main()
-'''for sg in ret:
-    labels=dict((n,d['name']) for n,d in sg.nodes(data=True)) 
-    nx.draw(sg, labels=labels,node_size=1000)
-    plt.show()
-    '''
-
-gList = File2Graph()
 
 
-#index = 0
-for sg in ret2:
-    labels=dict((n,d['name']) for n,d in sg.nodes(data=True)) 
-    nx.draw(sg, labels=labels,node_size=1000)
-    plt.show()
-    #plt.savefig("path_"+str(index)+".png")
-    #index += 1
-
-"""
